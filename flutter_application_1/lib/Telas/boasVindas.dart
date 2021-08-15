9b877ff5bf8976c9a7276048808b6cc1aaf3ea48
EvandroArmando <53578532+EvandroArmando@users.noreply.github.com>
2021-08-15 23:17:49 +0100

    up

---
+++ flutter_application_1/lib/Telas/boasVindas.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class BoasvindasPage extends StatelessWidget {
+  const BoasvindasPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("ola mundo"),
+    );
+  }
+}
