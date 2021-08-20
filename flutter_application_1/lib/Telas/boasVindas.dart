4efef82f1356ad55fe82e5296377dffd68f22568
EvandroArmando <53578532+EvandroArmando@users.noreply.github.com>
2021-08-20 02:20:13 +0100

    up

--- flutter_application_1/lib/Telas/boasVindas.dart
+++ flutter_application_1/lib/Telas/boasVindas.dart
@@ -1,7 +1,6 @@
 
 
 import 'dart:html';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/cores.dart';
--- flutter_application_1/lib/Telas/home.dart
+++ flutter_application_1/lib/Telas/home.dart
@@ -1,4 +1,5 @@
 
+import 'dart:html';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,7 @@ class HomePage extends StatelessWidget {
        Padding(
          padding: const EdgeInsets.all(16.0),
          child: Column(
+          crossAxisAlignment :CrossAxisAlignment.start,
            children: [
             Row(
               children: [
@@ -42,9 +44,49 @@ class HomePage extends StatelessWidget {
               ],
             ), 
             //cartaos
-            Row(
-               
-            )           
+            SizedBox(
+              height: 14,
+            ),
+            Column(
+               mainAxisAlignment: MainAxisAlignment.center,            
+              children: [
+                Row(
+                   children: [
+                     Row(
+
+                       children: [
+                         renderCartao(context, Icons.check_box_outlined, "100", "Concluidos", [verdeCima,verdeBaixa]),
+                         espacoHorizontal(),
+                         renderCartao(context, Icons.access_time_outlined, "12", "pendentes", [laranjaClaro,laranjaEscuro]),
+                         espacoHorizontal(),
+                       ], 
+                     ),
+                     
+                   ],
+                ) ,
+                espacoVertical(),
+                Row(
+                       children: [
+                         Row(
+                           children: [
+                             renderCartao(context, Icons.cancel_outlined, "2", "cancelados",[vermelhoCima,vermelhoBaixo]),
+                         espacoHorizontal(),
+                         renderCartao(context, Icons.person_rounded, "14", "em curso", [lilasCima,lilasBaixo])
+                           ],
+                         )
+                       ],
+                     ),
+              ],
+            ),
+
+          espacoVertical(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Agendamentos",style:TextStyle(fontSize:24)),
+              TextButton(onPressed:(){}, child: Text("ver mais",style: TextStyle(fontSize: 24),))
+            ],
+          )      
         ],
 
          ),
@@ -53,5 +95,57 @@ class HomePage extends StatelessWidget {
 
       ),
     );
+  }
+
+  SizedBox espacoVertical() {
+    return SizedBox(
+            
+            height: 12,
+          );
+  }
+
+  SizedBox espacoHorizontal() {
+    return SizedBox(
+                     width: 12,
+                   );
+  }
+
+  Container renderCartao(BuildContext context,IconData icone,String qtd,String nome,List<Color> cores) {
+    return Container(
+                 alignment: Alignment.center,
+                 height: 120,
+                 width: MediaQuery.of(context).size.width/2-32,
+                 decoration: BoxDecoration(
+                   gradient: LinearGradient(
+                    begin:Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                     colors: cores,
+                   ),
+                   borderRadius: BorderRadius.circular(
+                     18
+                   ),
+                 ),
+                 //icones e textos do cartao
+                 child: Row(
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(icone,color: Colors.white,size:24,),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      SizedBox(
+                        height: 58,
+                        child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [    
+                            Text(qtd,style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,color: Colors.white),),
+                            Text(nome,style: TextStyle(fontSize: 18,color: Colors.white30),)
+                          ],
+                        ),
+                      ),
+                    ],
+                 ),
+               );
   }
 }
